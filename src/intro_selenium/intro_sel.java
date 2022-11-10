a0f16630065b10ab8db99318322ac9bff14e7e54
undesicimo <a0ngel03@gmail.com>
2022-11-11 08:02:26 +0900

    deletions

--- src/intro_selenium/intro_sel.java
+++ src/intro_selenium/intro_sel.java
@@ -11,12 +11,7 @@ public class intro_sel {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\JPZ4373\\Documents\\chromedriv\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://trade-milestone-tradingview-alpha1.linesec-apps-beta.com/");
-		driver.findElement(By.className("XJp_Tw")).click();
-		driver.findElement(By.name("tid")).sendKeys("financial.ltk+76@gmail.com");
-		driver.findElement(By.name("tpasswd")).sendKeys("ltk0852");
-		driver.findElement(By.className("MdBtn01")).click();
-		driver.navigate().to("https://trade-milestone-tradingview-alpha1.linesec-apps-beta.com/stock/detail/6613/pro-chart");
+	
 		
 	}
 
